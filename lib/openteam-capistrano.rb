a9f717e5685563b532a23c9c4662bb8a7e808d66
Dmitry Lihachev <lda@openteam.ru>
2013-03-19 15:03:39 +0700

    initial commit

---
+++ lib/openteam-capistrano.rb
@@ -0,0 +1,7 @@
+require "openteam-capistrano/version"
+
+module Openteam
+  module Capistrano
+    # Your code goes here...
+  end
+end
---
+++ lib/openteam-capistrano/version.rb
@@ -0,0 +1,5 @@
+module Openteam
+  module Capistrano
+    VERSION = "0.0.1"
+  end
+end
