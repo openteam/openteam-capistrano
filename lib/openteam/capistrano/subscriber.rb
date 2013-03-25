d055178a9dfefc0581f19ff6acff910bf2df5fb4
Dmitry Lihachev <lda@openteam.ru>
2013-03-25 12:59:02 +0700

    simplify subscriber tasks

--- lib/openteam/capistrano/subscriber.rb
+++ lib/openteam/capistrano/subscriber.rb
@@ -1,23 +1,17 @@
 if File.exists?('script/subscriber')
   Capistrano::Configuration.instance.load do
-    # set subscriber hooks
     after 'deploy:update',      'subscriber:restart'
     after 'deploy:rollback',    'subscriber:restart'
 
     namespace :subscriber do
-      def subsriber_pid_file
-        "#{current_path}/tmp/pids/#{subscriber_name}.pid"
-      end
-
       desc "Stop rabbitmq subscriber"
       task :stop do
-        # subscriber will remove pid file automatically
-        run "if [ -s #{subsriber_pid_file} ]; then kill `cat #{subsriber_pid_file}`; fi"
+        run "RAILS_ENV=#{rails_env} #{current_path}/script/subscriber stop"
       end
 
       desc "Start rabbitmq subscriber"
       task :start do
-        run "RAILS_ENV=#{rails_env} #{current_path}/script/subscriber --name #{subscriber_name} start"
+        run "RAILS_ENV=#{rails_env} #{current_path}/script/subscriber start"
       end
 
       desc "Restart rabbitmq subscriber"
