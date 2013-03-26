d522c6afbb9c8100d5a196ab5e99abd284b343c5
Dmitry Lihachev <lda@openteam.ru>
2013-03-26 12:43:16 +0700

    remove openteam namespace form setup_deploy task due strange issues on storage deploy

--- lib/openteam/capistrano/setup_deploy.rb
+++ lib/openteam/capistrano/setup_deploy.rb
@@ -1,24 +1,22 @@
 Capistrano::Configuration.instance.load do
-  after 'multistage:ensure', 'openteam:setup_deploy'
+  after 'multistage:ensure', 'openteam_capistrano_setup_deploy'
 
-  namespace :openteam do
-    task :setup_deploy do
-      set :deploy_config,     YAML::load(File.open('config/deploy.yml'))[stage.to_s]
+  task :openteam_capistrano_setup_deploy do
+    set :deploy_config,     YAML::load(File.open('config/deploy.yml'))[stage.to_s]
 
-      # application name
-      set :application,       deploy_config['application']
+    # application name
+    set :application,       deploy_config['application']
 
-      # deployment host
-      set :domain,            deploy_config['domain']
+    # deployment host
+    set :domain,            deploy_config['domain']
 
-      #  deloyment gateway
-      set:gateway,            deploy_config['gateway'] if deploy_config.has_key? 'gateway'
+    #  deloyment gateway
+    set:gateway,            deploy_config['gateway'] if deploy_config.has_key? 'gateway'
 
-      # application root
-      set :deploy_to,         "/srv/#{application}"
+    # application root
+    set :deploy_to,         "/srv/#{application}"
 
-      # setup server
-      server domain, :app, :web, :db, :primary => true
-    end
+    # setup server
+    server domain, :app, :web, :db, :primary => true
   end
 end
