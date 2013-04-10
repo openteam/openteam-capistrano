a3bc7194fd21571bc392b53c3a5bae8f1ca8c10b
Dmitry Lihachev <lda@openteam.ru>
2013-04-10 13:02:53 +0700

    fix random wait replication freezes (when remote index changed during replication)

--- lib/openteam/capistrano/solr/solr.rb
+++ lib/openteam/capistrano/solr/solr.rb
@@ -5,7 +5,7 @@ require 'tmpdir'
 
 class Solr
   class Replicator
-    attr_accessor :remote, :local
+    attr_accessor :remote, :local, :base_local_index_version
 
     def initialize(options)
       self.remote = Solr.new options[:remote]
@@ -13,7 +13,8 @@ class Solr
     end
 
     def replicate
-      if same_versions?
+      self.base_local_index_version = local.index_version
+      if base_local_index_version == remote.index_version
         puts 'local and remote index versions are same, no need for replication'
       else
         really_replicate
@@ -24,7 +25,7 @@ class Solr
     def really_replicate
       print 'wait while solr replicated '
       local.send_replication_command :fetchindex, :masterUrl => "#{remote.url}/replication"
-      while !same_versions?
+      while base_local_index_version == local.index_version
         print '.'
         sleep 0.5
       end
@@ -35,10 +36,6 @@ class Solr
       STDOUT.print *args
       STDOUT.sync
     end
-
-    def same_versions?
-      remote.index_version == local.index_version
-    end
   end
 
   attr_accessor :url
@@ -48,7 +45,7 @@ class Solr
   end
 
   def index_version
-    send_replication_command(:indexversion)['indexversion']
+    send_replication_command(:indexversion)['indexversion'].to_i
   end
 
   def send_replication_command(command, extra={})
