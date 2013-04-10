cbbf889412e5b2e1f9bb2235f0da38bd3047a21f
Dmitry Lihachev <lda@openteam.ru>
2013-04-10 12:07:54 +0700

    warn if cannt find airbrake; auto set hook delayed_job:restart if delayed_job detected

--- lib/openteam/capistrano/dependencies.rb
+++ lib/openteam/capistrano/dependencies.rb
@@ -5,5 +5,19 @@ require 'rvm/capistrano'
 
 require 'capistrano-db-tasks' if use_db?
 require 'capistrano-unicorn'
-begin; require 'airbrake/capistrano'; rescue LoadError; end
+
+# airbrake
+begin
+  require 'airbrake/capistrano'
+rescue LoadError
+  STDERR.puts '!!! WARNING: airbrake not detected, deployment notifications has been disabled !!!'
+end
+
+
+# delayed job
+begin
+  require 'delayed/recipes'
+  Capistrano::Configuration.instance.load { after 'deploy:restart', 'delayed_job:restart' }
+rescue LoadError
+end
 
