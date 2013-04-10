aca2e9b47e8e11991a7cd0013418ebca06c4695b
Dmitry Lihachev <lda@openteam.ru>
2013-04-10 12:51:33 +0700

    keep only fresh stage tags

--- lib/openteam/capistrano/tagging.rb
+++ lib/openteam/capistrano/tagging.rb
@@ -5,18 +5,28 @@ Capistrano::Configuration.instance.load do
   namespace :tagging do
     def tag_name(options={})
       formatted_local_time = DateTime.strptime(options[:for], '%Y%m%d%H%M%S').to_time.strftime("%Y.%m.%d-%H%M")
-      "#{fetch(:stage)}-#{formatted_local_time}"
+      "#{stage}-#{formatted_local_time}"
     end
 
     def create_tag(tag_name)
       run_locally "git tag -a #{tag_name} -m 'Deployed by #{fetch(:local_user)}' origin/#{fetch(:branch)}"
-      run_locally "git push origin --tags"
+      run_locally "git push origin #{tag_name}"
     end
 
-    def remove_tag(tag_name)
-      run_locally "git tag -d #{tag_name} || true"
-      run_locally "git push origin :refs/tags/#{tag_name}"
+    def remove_tags(tags)
+      tag_refs = tags.map{|tag| ":refs/tags/#{tag}"}
+      run_locally "git tag -d #{tags.join(' ')}"
+      run_locally "git push origin #{tag_refs.join(' ')}"
     end
+
+    set(:stage_tags) do
+      run_locally('git fetch --tags')
+      run_locally("git tag -l | grep ^#{stage}-").chomp.split("\n")
+    end
+    set(:kept_releases_count) { fetch(:keep_releases, 5) }
+    set(:kept_releases) { releases.last(kept_releases_count) }
+    set(:kept_tags) { kept_releases.map{|release| tag_name(:for => release)} }
+    set(:rotten_tags) { stage_tags - kept_tags }
 
     desc "Create release tag in local and origin repo"
     task :deploy do
@@ -26,16 +36,11 @@ Capistrano::Configuration.instance.load do
 
     desc "Remove release tag from local and origin repo"
     task :cleanup do
-      run_locally 'git fetch --tags'
-      count = fetch(:keep_releases, 5)
-
-      if count >= releases.size
-        logger.important "no old release tags to clean up"
+      if rotten_tags.any?
+        logger.info "keeping #{kept_releases_count} of #{stage} #{stage_tags.size} stage tags"
+        remove_tags(rotten_tags)
       else
-        logger.info "keeping #{count} of #{releases.size} release tags"
-        releases.first(releases.size - count).map do |release|
-          remove_tag tag_name(:for => release)
-        end
+        logger.important "no old release tags to clean up"
       end
     end
   end
